25a5601a2f31e59dcbc1e270399d601a5d689a7d
Ngoguey42 <ngoguey@student.42.fr>
2020-02-20 22:02:53 +0100

    Minor changes

--- bin/lrcraft.ml
+++ bin/lrcraft.ml
@@ -1,9 +1,18 @@
-module Firebug = Js_of_ocaml.Firebug
+module Dom_html = Js_of_ocaml.Dom_html
+module Dom = Js_of_ocaml.Dom
+module Tyxml_js = Js_of_ocaml_tyxml.Tyxml_js
 module Html = Js_of_ocaml_tyxml.Tyxml_js.Html
 module Js = Js_of_ocaml.Js
+module Firebug = Js_of_ocaml.Firebug
 
-module AlgodiffD = Owl_algodiff_generic.Make (Owl_base_dense_ndarray.D)
 module NeuralD = Owl_neural_generic.Make(Owl_base_dense_ndarray.D)
+
+module Aa = Irm
+
+(* let document = Dom_html.window##.document *)
+(* let body = Dom_html.window##.document##.body *)
+
+(* let display x = Dom.appendChild body (Tyxml_js.To_dom.of_element x) *)
 
 let network =
   let n =
@@ -25,15 +34,41 @@ let x =
   NeuralD.Algodiff.Arr.uniform ~a:~-.1. ~b:1. [|2;7;7;1|]
 
 let shp = NeuralD.Algodiff.Arr.shape x
-let y0, y1 = NeuralD.Graph.forward network x
+let y, y1 = NeuralD.Graph.forward network x
+
+module Ft = struct
+  module List = struct
+    let rec string_join sep = function
+      | a::(_::_ as tail) -> a ^ sep ^ string_join sep tail
+      | [a] -> a
+      | [] -> ""
+  end
+end
+
+module Str = struct
+  let shape arr =
+    NeuralD.Algodiff.Arr.shape arr
+    |> Array.map string_of_int
+    |> Array.to_list
+    |> Ft.List.string_join ", "
+    |> Printf.sprintf "(%s)"
+
+  let array arr =
+    NeuralD.Algodiff.unpack_arr arr
+    |> Owl_pretty.dsnda_to_string
+         ~header:false
+         ~max_row:1000
+         ~max_col:1000
+end
 
 let _ =
+
   Firebug.console##log "x";
   Firebug.console##log (Js.array @@ NeuralD.Algodiff.Arr.shape x);
 
-  Firebug.console##log "y0";
-  Firebug.console##log (Js.array @@ NeuralD.Algodiff.Arr.shape y0);
-  print_arr_ad y0;
+  Firebug.console##log "y";
+  Firebug.console##log (Js.array @@ NeuralD.Algodiff.Arr.shape y);
+  print_arr_ad y;
 
   (* Firebug.console##log "y1"; *)
   (* Firebug.console##log (Array.length y1); *)
@@ -47,10 +82,15 @@ let _ =
   (*     Firebug.console##log (Js.array @@ NeuralD.Algodiff.Arr.shape b); *)
   (*   done *)
   (* done; *)
-
   ()
 
 let create_content () =
-  [%html{|
-         <div></div>
-         |}]
+  let l = [
+      Str.shape y |> Printf.sprintf "y shape: %s" |> Html.txt;
+      [%html "<br/>"];
+      Str.shape y |> Printf.sprintf "y shape: %s" |> Html.txt;
+      [%html "<br/>"];
+      Printf.sprintf "y" |> Html.txt;
+      Str.array y |> Html.txt;
+    ] in
+  Html.div l

abf5c4370cb3ec54901d27f8e2472ef46ced8801
Ngoguey42 <ngoguey@student.42.fr>
2020-06-04 15:39:32 +0200

    Repair forward and pack in training-owl

--- lib/fnn_owl/bridge.ml
+++ lib/fnn_owl/bridge.ml
@@ -42,7 +42,7 @@ type accumulator = {
   pack : unit -> Fnn.network;
 }
 
-let _unpack_node01 (net : Fnn.node01) =
+let _unpack_layer01 (net : Fnn.node01) =
   match net#classify_layer with
   | `Input _ ->
       let net = (net :> Fnn.network) in
@@ -52,7 +52,7 @@ let _unpack_node01 (net : Fnn.node01) =
         validate_output_tensor net tensor
       in
       let pack () = net in
-      { optimizations = OptiMap.empty; forward; pack }
+      forward, pack, None
   | `Parameter32 net ->
       let var = ref (net#tensor |> Algodiff.pack_arr |> Fun.flip Algodiff.make_reverse 42) in
       let forward _ =
@@ -65,35 +65,33 @@ let _unpack_node01 (net : Fnn.node01) =
         let w = !var |> Algodiff.primal' |> Algodiff.unpack_arr in
         (net#replicate ~id:net#id w optimizer :> Fnn.network)
       in
-      { optimizations = OptiMap.singleton (string_of_int (Oo.id net)) update; forward; pack }
+      forward, pack, Some update
 
-let _unpack_layer11 (net : Fnn.node11) up_forward =
+let _unpack_layer11 (net : Fnn.node11) =
   match net#classify_layer with
   | `Relu _ ->
-      let forward inputs =
+      let forward up =
         Printf.eprintf "forward relu\n%!";
-        validate_output_tensor net (Algodiff.Maths.relu (up_forward inputs))
+        validate_output_tensor net (Algodiff.Maths.relu up)
       in
-      let copy : Fnn.network list -> Fnn.network = (net :> Fnn.network)#copy in
-      (forward, copy)
+      let pack up = (net :> Fnn.network)#copy [ up ] in
+      (forward, pack)
   | `Softmax net ->
       let tensor_axis = tensor_axis_of_shape_axis net#upstream#out_shape net#axis in
-      let forward inputs =
+      let forward up =
         Printf.eprintf "forward softmax a\n%!";
-
-        let x = up_forward inputs in
         let max =
-          x |> Algodiff.primal' |> Algodiff.unpack_arr |> Algodiff.A.max ~axis:tensor_axis
+          up |> Algodiff.primal' |> Algodiff.unpack_arr |> Algodiff.A.max ~axis:tensor_axis
           |> Algodiff.pack_arr
         in
-        let x = Algodiff.Maths.sub x max in
+        let x = Algodiff.Maths.sub up max in
         let x = Algodiff.Maths.exp x in
         let deno = Algodiff.Maths.sum ~axis:tensor_axis x in
         let x = Algodiff.Maths.div x deno in
         validate_output_tensor net x
       in
-      let copy : Fnn.network list -> Fnn.network = (net :> Fnn.network)#copy in
-      (forward, copy)
+      let pack up = (net :> Fnn.network)#copy [ up ] in
+      (forward, pack)
   | `Astype _ -> failwith "`Astype is unsupported with Owl backend"
   | `Padding net ->
       (* if net#is_mixed then failwith "Mixed paddings not yet implemented"; *)
@@ -127,12 +125,12 @@ let _unpack_layer11 (net : Fnn.node11) up_forward =
         (*   in *)
         (*   Tfjs_api.Ops.slice per_axis x *)
       in
-      let forward inputs =
+      let forward up =
         Printf.eprintf "forward padding\n%!";
-        up_forward inputs |> f |> validate_output_tensor net
+        up |> f |> validate_output_tensor net
       in
-      let copy : Fnn.network list -> Fnn.network = (net :> Fnn.network)#copy in
-      (forward, copy)
+      let pack up = (net :> Fnn.network)#copy [ up ] in
+      (forward, pack)
   | `Maxpool2d net ->
       let b =
         match net#boundary_mode with
@@ -149,13 +147,12 @@ let _unpack_layer11 (net : Fnn.node11) up_forward =
         let sy, sx = net#stride in
         [| sy; sx |]
       in
-      let forward inputs =
+      let forward up =
         Printf.eprintf "forward maxpool\n%!";
-        let x = up_forward inputs in
-        Algodiff.NN.max_pool2d b x kernel_size s |> validate_output_tensor net
+        Algodiff.NN.max_pool2d b up kernel_size s |> validate_output_tensor net
       in
-      let copy : Fnn.network list -> Fnn.network = (net :> Fnn.network)#copy in
-      (forward, copy)
+      let pack up = (net :> Fnn.network)#copy [ up ] in
+      (forward, pack)
   | `Transpose net ->
       let transpose_axes, dims1_of_dims0 = derive_configuration_of_transpose_layer net in
       if transpose_axes <> [ 0; 1; 2; 3 ] && transpose_axes <> [ 0 ] then (
@@ -164,28 +161,17 @@ let _unpack_layer11 (net : Fnn.node11) up_forward =
         Printf.eprintf "%s\n%!" (Pshape.to_string net#out_shape);
         transpose_axes |> List.map string_of_int |> String.concat ", " |> print_endline;
         failwith "Unsupported transpose. Awaiting next owl-base version for `transpose ~axis`" );
-      let forward inputs =
+      let forward up =
         Printf.eprintf "Forward transpose\n%!";
-        let x = up_forward inputs in
-        let reshape_shape = x |> Algodiff.primal' |> Algodiff.Arr.shape |> dims1_of_dims0 in
+        let reshape_shape = up |> Algodiff.primal' |> Algodiff.Arr.shape |> dims1_of_dims0 in
         (* Algodiff.Maths.transpose ~axis:transpose_axes x *)
-        Algodiff.Maths.reshape x reshape_shape |> validate_output_tensor net
+        Algodiff.Maths.reshape up reshape_shape |> validate_output_tensor net
       in
-      let copy : Fnn.network list -> Fnn.network = (net :> Fnn.network)#copy in
-      (forward, copy)
+      let pack up = (net :> Fnn.network)#copy [ up ] in
+      (forward, pack)
   | `Normalisation _ -> failwith "Not yet implemented"
 
-(*     let shape = net#upstream#out_shape in *)
-(*     let axes = List.map (tensor_axis_of_shape_axis shape) net#axes in *)
-(*     let norm_forward, norm_pack = _unpack_normalisation_algorithm axes net#algorithm in *)
-(*     let forward inputs = up_forward inputs |> norm_forward |> validate_output_tensor net in *)
-(*     let copy = function *)
-(*       | [ upstream ] -> (net#replicate (norm_pack ()) upstream :> Fnn.network) *)
-(*       | _ -> failwith "unreachable: Normalisation.pack takes only 1 input" *)
-(*     in *)
-(*     (forward, copy) *)
-
-let _unpack_layer21 (net : Fnn.node21) up0_forward up1_forward =
+let _unpack_layer21 (net : Fnn.node21) =
   match net#classify_layer with
   | `Conv2d net ->
       if net#is_grouped then failwith "Grouped conv2d not implemented";
@@ -197,17 +183,17 @@ let _unpack_layer21 (net : Fnn.node21) up0_forward up1_forward =
         | `Assert_fit -> Owl_types.VALID
         | `Pad_fit -> failwith "Conv2d ~b:`Pad_fit not implemented"
       in
-      (* let d = net#dilation in *)
       let s =
         let sy, sx = net#stride in
         [| sy; sx |]
       in
-      let forward inputs =
+      let forward up0 up1 =
         Printf.eprintf "forward conv2d\n%!";
-        Algodiff.NN.conv2d ~padding:b (up1_forward inputs) (up0_forward inputs) s
+        Algodiff.NN.conv2d ~padding:b up1 up0 s
         |> validate_output_tensor net
       in
-      (forward, (net :> Fnn.network)#copy)
+      let pack up0 up1 = (net :> Fnn.network)#copy [ up0; up1 ] in
+      (forward, pack)
   | `Tensordot net ->
       let mapping, perm = derive_configuration_of_tensordot_layer net in
       if mapping <> [ (1, 0) ] then failwith "Tensordot only supported as dot";
@@ -215,18 +201,19 @@ let _unpack_layer21 (net : Fnn.node21) up0_forward up1_forward =
         failwith
           "Unsupported transpose in tensordot. Awaiting next owl-base version for `transpose ~axis`";
 
-      let forward inputs =
+      let forward up0 up1 =
         Printf.eprintf "forward tensordot\n%!";
-        Algodiff.Maths.dot (up0_forward inputs) (up1_forward inputs)
+        Algodiff.Maths.dot up0 up1
         (* |> Tfjs_api.Ops.transpose ~perm *)
         |> validate_output_tensor net
       in
-      (forward, (net :> Fnn.network)#copy)
+      let pack up0 up1 = (net :> Fnn.network)#copy [ up0; up1 ] in
+      (forward, pack)
 
-let _unpack_layern1 (net : Fnn.noden1) up_forwards =
+let _unpack_layern1 (net : Fnn.noden1) =
   match net#classify_layer with
   | `Sum net ->
-      let forward inputs =
+      let forward ups =
         Printf.eprintf "forward sum\n%!";
         let rec aux = function
           | [] -> failwith "unreachable (sum layer without upstream parents)"
@@ -235,59 +222,88 @@ let _unpack_layern1 (net : Fnn.noden1) up_forwards =
               let x = Algodiff.Maths.add x x' in
               aux (x :: tl)
         in
-        List.map (fun fn -> fn inputs) up_forwards |> aux |> validate_output_tensor net
+        ups |> aux |> validate_output_tensor net
       in
       (forward, (net :> Fnn.network)#copy)
   | `Prod net ->
       Printf.eprintf "forward prod\n%!";
-      let forward inputs =
+      let forward ups =
         let rec aux = function
           | [] -> failwith "unreachable (sum layer without upstream parents)"
           | [ x ] -> x
           | x :: x' :: tl -> aux (Algodiff.Maths.mul x x' :: tl)
         in
-        List.map (fun fn -> fn inputs) up_forwards |> aux |> validate_output_tensor net
+        ups |> aux |> validate_output_tensor net
       in
       (forward, (net :> Fnn.network)#copy)
   | `Concatenate net ->
       let axis = tensor_axis_of_shape_axis net#out_shape net#axis in
-      let forward inputs =
+      let forward ups =
         Printf.eprintf "forward concat\n%!";
-        List.map (fun fn -> fn inputs) up_forwards
+        ups
         |> Array.of_list |> Algodiff.Maths.concatenate ~axis |> validate_output_tensor net
       in
       (forward, (net :> Fnn.network)#copy)
 
 let _unpack_node follow (net : Fnn.network) =
   match (net#classify_node, List.map follow net#upstreams) with
-  | `Node01 net, [] -> _unpack_node01 net
-  | `Node11 net, [ up_acc ] ->
-      let forward, pack = _unpack_layer11 net up_acc.forward in
-      let pack () = (pack [ up_acc.pack () ] :> Fnn.network) in
-      { up_acc with forward; pack }
-  | `Node21 net, [ up0_acc; up1_acc ] ->
-      let forward, pack = _unpack_layer21 net up0_acc.forward up1_acc.forward in
-      let pack () = (pack [ up0_acc.pack (); up1_acc.pack () ] :> Fnn.network) in
-      let optimizations = OptiMap.union_silent up0_acc.optimizations up1_acc.optimizations in
-      { forward; optimizations; pack }
-  | `Noden1 net, up_accs ->
-      let up_forwards = List.map (fun acc -> acc.forward) up_accs in
-      let up_packs = List.map (fun acc -> acc.pack) up_accs in
-      let up_optimizations = List.map (fun acc -> acc.optimizations) up_accs in
-      let optimizations = OptiMap.union_list_silent up_optimizations in
-      let forward, pack = _unpack_layern1 net up_forwards in
-      let pack () = List.map (fun pack -> pack ()) up_packs |> pack in
-      { forward; optimizations; pack }
+  | `Node01 node, [] ->
+      let forward, pack, update = _unpack_layer01 node in
+      Node01 { forward; pack; update; node = (node :> Fnn.network) }
+  | `Node11 node, [ up ] ->
+      let forward, pack = _unpack_layer11 node in
+      Node11 { forward; pack; node = (node :> Fnn.network); up }
+  | `Node21 node, [ up0; up1 ] ->
+      let forward, pack = _unpack_layer21 node in
+      Node21 { forward; pack; node = (node :> Fnn.network); up0; up1 }
+  | `Noden1 node, ups ->
+      let forward, pack = _unpack_layern1 node in
+      Noden1 { forward; pack; node = (node :> Fnn.network); ups }
   | _, _ -> failwith "Corrupted network. A node has an unexpected number of upstream parents"
 
 let unpack_for_training :
     Fnn.network -> (Algodiff.t Fnn.Map.t -> Algodiff.t) * optimization_map * (unit -> Fnn.network) =
  (* Transform a `network` to everything that is needed to perform a training of that network
   * using Owl algodiff ndarray:
-  * 1. A callable for the forward pass that provides closures to update the weights after the
-         backward pass
-  * 2. A thunk to be called to pack everything back to a `network` when done with training
+  * 1. A callable for the forward pass aware of the upcoming backward pass
+  * 2. A map of callbacks that each update a weight tensor given its gradient
+  * 3. A thunk to be called to pack everything back to a `network` when done with training
   *)
  fun net ->
-  let { forward; optimizations; pack } = Fnn.memoized_walk_obj _unpack_node net in
+  let unet = Fnn.memoized_walk_obj _unpack_node net in
+  let id_of_unet = function
+    | Node01 v -> Oo.id v.node
+    | Node11 v -> Oo.id v.node
+    | Node21 v -> Oo.id v.node
+    | Noden1 v -> Oo.id v.node
+  in
+  let forward inputs =
+    let forward_node follow = function
+      | Node01 v -> v.forward inputs
+      | Node11 v -> v.forward (follow v.up)
+      | Node21 v -> v.forward (follow v.up0) (follow v.up1)
+      | Noden1 v -> v.forward (List.map follow v.ups)
+    in
+    Fnn.memoized_walk id_of_unet forward_node unet
+  in
+  let pack () =
+    let pack_node follow = function
+      | Node01 v -> v.pack ()
+      | Node11 v -> v.pack (follow v.up)
+      | Node21 v -> v.pack (follow v.up0) (follow v.up1)
+      | Noden1 v -> v.pack (List.map follow v.ups)
+    in
+    Fnn.memoized_walk id_of_unet pack_node unet
+  in
+  let optimizations =
+    let optimizations_of_node follow = function
+      | Node01 { update = None; _ } -> OptiMap.empty
+      | Node01 { update = Some update; node; _ } ->
+          OptiMap.singleton (string_of_int (Oo.id node)) update
+      | Node11 v -> follow v.up
+      | Node21 v -> OptiMap.union_silent (follow v.up0) (follow v.up1)
+      | Noden1 v -> List.map follow v.ups |> OptiMap.union_list_silent
+    in
+    Fnn.memoized_walk id_of_unet optimizations_of_node unet
+  in
   (forward, optimizations, pack)
